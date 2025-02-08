38da49f7d2badd505ac46ffaef4c988fc5cdbd3f
jhuynh <79059047+John5675@users.noreply.github.com>
2025-02-08 14:10:45 -0800

    Setup Starting Routes and Connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import connectDB from "./lib/connectDB.js";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.comment.js";
+import commentRouter from "./routes/comment.route.js";
 
 const app = express();
 
-console.log("Changed2");
+// app.get("/test", (req, res) => {
+//   res.status(200).send("it works");
+// });
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running");
 });
