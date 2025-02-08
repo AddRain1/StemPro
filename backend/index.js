5a318dd71068bdda7d138ad2e57e88fd642530e3
jhuynh <79059047+John5675@users.noreply.github.com>
2025-02-08 13:47:22 -0800

    Edited Index folder

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,9 @@ import express from "express";
 import connectDB from "./lib/connectDB.js";
 
 const app = express();
+
+console.log("Changed2");
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
