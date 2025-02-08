d38b32b942c3383e2d0d65d621c8170aaaf2e5fa
jhuynh <79059047+John5675@users.noreply.github.com>
2025-02-08 14:14:27 -0800

    Fixed Schemas

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -2,23 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // Assuming you have a User model
-        required: true
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Assuming you have a User model
+    required: true,
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  replies: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
     },
-    text: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    replies: [{
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-    }]
+  ],
 });
 
 module.exports = mongoose.models("Comment", commentSchema);
--- backend/models/post.js
+++ backend/models/post.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    subject: {
-        type: String,
-        required: true
-    },
-    question: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    answer: {
-        type: String,
-        required: true
-    },
-    comments: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-    }
-})
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  comments: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment",
+  },
+});
 
 module.exports = mongoose.model("Post", postSchema);
