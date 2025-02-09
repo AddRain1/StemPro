a0299fec59756e8fbc6c2eda4d5b6a7833ccaf65
jhuynh <79059047+John5675@users.noreply.github.com>
2025-02-08 21:15:54 -0800

    Completed Sorting Questions By Subject

--- frontend/stempro/src/Routes.js
+++ frontend/stempro/src/Routes.js
@@ -1,29 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from './components/HomePage.js';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import HomePage from "./components/HomePage.js";
 import { NotFoundPage } from "./components/NotFoundPage.js";
-import { ComputerScience } from "./components/ComputerScience.js";
-import { Biology } from "./components/Biology.js";
-import { Chemistry } from "./components/Chemistry.js";
-import { Engineering } from "./components/Engineering.js";
-import { Mathematics } from "./components/Mathematics.js";
-import { Physics } from "./components/Physics.js";
+import { ProblemListPage } from "./components/ProblemListPage.js";
 import ProblemDetail from "./components/ProblemDetail.js";
 
 export const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" exact element={<Navigate to="/homepage" />} />
-                <Route path="/homepage" exact element={<HomePage />} />
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path="/computerscience" element={<ComputerScience />} />
-                <Route path="/computerscience/problem/:id/*" element={<ProblemDetail />} />
-                <Route path="/biology" element={<Biology />} />
-                <Route path="/chemistry" element={<Chemistry />} />
-                <Route path="/engineering" element={<Engineering />} />
-                <Route path="/mathematics" element={<Mathematics />} />
-                <Route path="/physics" element={<Physics />} />
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to="/homepage" />} />
+        <Route path="/homepage" exact element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/:subject" element={<ProblemListPage />} />
+        <Route path="/:subject/problem/:id/*" element={<ProblemDetail />} />
+      </Routes>
+    </Router>
+  );
+};
--- frontend/stempro/src/components/HomePage.js
+++ frontend/stempro/src/components/HomePage.js
@@ -1,45 +1,56 @@
-import React from 'react';
-import './HomePage.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleComputerScience = () => {
+    navigate("/computer-science");
+  };
+  const handleEngineering = () => {
+    navigate("/engineering");
+  };
+  const handleMathematics = () => {
+    navigate("/mathematics");
+  };
+  const handlePhysics = () => {
+    navigate("/physics");
+  };
+  const handleBiology = () => {
+    navigate("/biology");
+  };
+  const handleChemistry = () => {
+    navigate("/chemistry");
+  };
 
-    const handleComputerScience = () => {
-        navigate('/computerscience');
-    }
-    const handleEngineering = () => {
-        navigate('/engineering');
-    }
-    const handleMathematics = () => {
-        navigate('/mathematics');
-    }
-    const handlePhysics = () => {
-        navigate('/physics');
-    }
-    const handleBiology = () => {
-        navigate('/biology');
-    }
-    const handleChemistry = () => {
-        navigate('/chemistry');
-    }
-
-    return (
-        <div className="homepage-container">
-            <div className="homepage-box">
-                <h1 className="homepage-title">Welcome to StemPro!</h1>
-                    <div className="button-container">
-                        <button className="homepage-button" onClick={handleComputerScience}>Computer Science</button>
-                        <button className="homepage-button" onClick={handleEngineering}>Engineering</button>
-                        <button className="homepage-button" onClick={handleMathematics}>Mathematics</button>
-                        <button className="homepage-button" onClick={handleChemistry}>Chemistry</button>
-                        <button className="homepage-button" onClick={handlePhysics}>Physics</button>
-                        <button className="homepage-button" onClick={handleBiology}>Biology</button>
-                    </div>
-            </div>
+  return (
+    <div className="homepage-container">
+      <div className="homepage-box">
+        <h1 className="homepage-title">Welcome to StemPro!</h1>
+        <div className="button-container">
+          <button className="homepage-button" onClick={handleComputerScience}>
+            Computer Science
+          </button>
+          <button className="homepage-button" onClick={handleEngineering}>
+            Engineering
+          </button>
+          <button className="homepage-button" onClick={handleMathematics}>
+            Mathematics
+          </button>
+          <button className="homepage-button" onClick={handleChemistry}>
+            Chemistry
+          </button>
+          <button className="homepage-button" onClick={handlePhysics}>
+            Physics
+          </button>
+          <button className="homepage-button" onClick={handleBiology}>
+            Biology
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
